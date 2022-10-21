6591c8e000e0974a0aa1d13b66901910fe3a9367
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2022-10-21 18:25:36 +0300

    First step

---
+++ app/src/main/java/com/example/orionplus/Materials.kt
@@ -0,0 +1,11 @@
+package com.example.orionplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Materials : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materials2)
+    }
+}
